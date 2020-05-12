5ce6dc8eefc8ea0eb4adea0fa77f909d6f2b9ac0
James Robertson <james@jamesrobertson.eu>
2020-05-12 23:08:07 +0100

    feature: The record method by default saves a snapshot every second for a duration of 6 seconds.

--- lib/x4ss.rb
+++ lib/x4ss.rb
@@ -3,30 +3,78 @@
 # file: x4ss.rb
 
 # A wrapper for xfce4-screenshooter and xclip to save screenshots
+# Looking for a simple command-line progam? Try scrot.
+
+require 'c32'
+
 
 class X4ss
+  using ColouredText
 
-  def initialize(savefile=nil, mouse: false)
+  def initialize(savefile=nil, mouse: false, window: false, debug: false)
 
     @file = savefile
     @count = 1
-    @mouse = mouse ? 't' : ''
+    @mouse = mouse ? 'm' : ''
+    @blob = []
+    @mode = window ? :window : :fullscreen
+    @debug = debug
 
   end
 
   # mode options: :window, :fullscreen
   #
-  def capture(mode=:fullscreen)
+  def capture(mode=@mode)
+    
+    blob = screenshot(mode)
+    
+    if @file then
+    
+      File.write @file % @count, blob
+      
+    end
 
-    area = mode == :fullscreen ? 'f' : 'w'
-    `xfce4-screenshooter -c#{area}#{@mouse}`
-    @blob = `xclip -selection clipboard -target image/png -out`
-    File.write @file % @count, @blob
-    @count += 1
+    @count += 1    
+    @blob << blob
+
   end
 
   def from_blob
     @blob
   end
 
+  def record(interval: 1, duration: 6, mode: @mode)
+    
+    return 'interval must be 0.8 or greater' if interval < 0.8
+
+    t = Time.now
+
+    while(Time.now <= t+duration) do
+
+      puts 'time: ' + Time.now.inspect
+      @blob << screenshot(mode)
+      sleep interval
+      yield(@blob.shift) if block_given?
+
+    end
+
+  end
+
+  def save(file=@file)
+    
+    @blob.each.with_index {|blob, i| File.write file % (i+1).to_s, blob }
+        
+    :saved
+  end
+  
+  private
+  
+  def screenshot(mode=@mode)
+    
+    area = mode == :fullscreen ? 'f' : 'w'
+    
+    `xfce4-screenshooter -c#{area}#{@mouse}`
+    `xclip -selection clipboard -target image/png -out`    
+    
+  end
 end
--- x4ss.gemspec
+++ x4ss.gemspec
@@ -1,9 +1,10 @@
 Gem::Specification.new do |s|
   s.name = 'x4ss'
-  s.version = '0.1.1'
+  s.version = '0.2.0'
   s.summary = 'A wrapper for xfce4-screenshooter and xclip to save screenshots.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/x4ss.rb']
+  s.add_runtime_dependency('c32', '~> 0.2', '>=0.2.0') 
   s.signing_key = '../privatekeys/x4ss.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
